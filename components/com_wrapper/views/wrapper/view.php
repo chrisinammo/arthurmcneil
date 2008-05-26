7a9a3e1dc34e704092bc7bac0536fc4f5867cb72
Chris French <chris@inammo.com>
2008-05-26 16:13:21 -0400

    pushing joomla files

---
+++ components/com_wrapper/views/wrapper/view.php
@@ -0,0 +1,38 @@
+<?php
+/**
+* @version		$Id: view.php 10094 2008-03-02 04:35:10Z instance $
+* @package		Joomla
+* @subpackage	Wrapper
+* @copyright	Copyright (C) 2005 - 2008 Open Source Matters. All rights reserved.
+* @license		GNU/GPL, see LICENSE.php
+* Joomla! is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See COPYRIGHT.php for copyright notices and details.
+*/
+
+// Check to ensure this file is included in Joomla!
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+jimport( 'joomla.application.component.view');
+
+/**
+* @package		Joomla
+* @subpackage	Wrapper
+*/
+class WrapperViewWrapper extends JView
+{
+	function display( $tpl = null )
+	{
+		// auto height control
+		if ( $this->params->def( 'height_auto' ) ) {
+			$this->wrapper->load = 'onload="iFrameHeight()"';
+		} else {
+			$this->wrapper->load = '';
+		}
+
+		parent::display($tpl);
+	}
+}
+?>
---
+++ modules/mod_login/helper.php
@@ -0,0 +1,42 @@
+<?php
+/**
+* @version		$Id: helper.php 10215 2008-04-19 09:28:27Z eddieajau $
+* @package		Joomla
+* @copyright	Copyright (C) 2005 - 2008 Open Source Matters. All rights reserved.
+* @license		GNU/GPL, see LICENSE.php
+* Joomla! is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See COPYRIGHT.php for copyright notices and details.
+*/
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+class modLoginHelper
+{
+	function getReturnURL($params, $type)
+	{
+		if($itemid =  $params->get($type))
+		{
+			$menu =& JSite::getMenu();
+			$item = $menu->getItem($itemid);
+			$url = $item->link;
+		}
+		else
+		{
+			// Redirect to login
+			$uri = JFactory::getURI();
+			$url = $uri->toString();
+		}
+
+		return base64_encode($url);
+	}
+
+	function getType()
+	{
+		$user = & JFactory::getUser();
+		return (!$user->get('guest')) ? 'logout' : 'login';
+	}
+}
