ca15fdfd67cc26b154179d31b4541920a3cf308f
Chris French <chris@inammo.com>
2008-06-15 23:55:00 -0400

    installed and updated joomla.
    installed database basic content,
    
    main template basic design worked out.  very close to  design.
    
    added basic functionality to template.

---
+++ configuration.php
@@ -0,0 +1,66 @@
+<?php
+class JConfig {
+/* Site Settings */
+var $offline = '0';
+var $offline_message = 'This site is down for maintenance.<br /> Please check back again soon.';
+var $sitename = 'ArthurMcneil';
+var $editor = 'tinymce';
+var $list_limit = '20';
+var $legacy = '0';
+/* Debug Settings */
+var $debug = '0';
+var $debug_lang = '0';
+/* Database Settings */
+var $dbtype = 'mysql';
+var $host = 'localhost:8889';
+var $user = 'root';
+var $password = 'root';
+var $db = 'arthurmcneil';
+var $dbprefix = 'jos_';
+/* Server Settings */
+var $live_site = '';
+var $secret = 'Wlsv3bj0ESIR1mFv';
+var $gzip = '0';
+var $error_reporting = '-1';
+var $helpurl = 'http://help.joomla.org';
+var $xmlrpc_server = '0';
+var $ftp_host = '127.0.0.1';
+var $ftp_port = '21';
+var $ftp_user = '';
+var $ftp_pass = '';
+var $ftp_root = '';
+var $ftp_enable = '0';
+/* Locale Settings */
+var $offset = '0';
+var $offset_user = '0';
+/* Mail Settings */
+var $mailer = 'mail';
+var $mailfrom = 'chris@theenrecords.com';
+var $fromname = 'ArthurMcneil';
+var $sendmail = '/usr/sbin/sendmail';
+var $smtpauth = '0';
+var $smtpuser = '';
+var $smtppass = '';
+var $smtphost = 'localhost';
+/* Cache Settings */
+var $caching = '0';
+var $cachetime = '15';
+var $cache_handler = 'file';
+/* Meta Settings */
+var $MetaDesc = 'Joomla! - the dynamic portal engine and content management system';
+var $MetaKeys = 'joomla, Joomla';
+var $MetaTitle = '1';
+var $MetaAuthor = '1';
+/* SEO Settings */
+var $sef           = '0';
+var $sef_rewrite   = '0';
+var $sef_suffix    = '0';
+/* Feed Settings */
+var $feed_limit   = 10;
+var $log_path = '/Users/chris/Sites/arthurmcneil.com/html/logs';
+var $tmp_path = '/Users/chris/Sites/arthurmcneil.com/html/tmp';
+/* Session Setting */
+var $lifetime = '15';
+var $session_handler = 'database';
+}
+?>
